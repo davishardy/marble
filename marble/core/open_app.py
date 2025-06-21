3a9e3f180eb91c9ce0f2ce2aaf26055e26f66d6d
Davis Hardy <davischardy@gmail.com>
2025-06-21 04:24:17 -0500

    open apps, currently only compatible with mac paths

---
+++ marble/core/open_app.py
@@ -0,0 +1,10 @@
+import os
+
+def open_blender():
+    os.system("open -a /Applications/Blender.app")
+
+def open_houdini():
+    os.system("open -a /Applications/Houdini/Houdini20.5.278/Houdini\ FX\ 20.5.278.app")
+
+def open_nuke():
+    os.system("open -a /Applications/Nuke15.1v4/NukeX15.1v4.app")
